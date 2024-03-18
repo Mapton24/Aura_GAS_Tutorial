1a2fa774505e687c1684edbabd5bfcbab57a8569
Mapton24 <robertosk555@gmail.com>
2024-03-18 14:06:14 +0100

    Created CharacterClassInfo class to store different classes of enemies with different attributes

---
+++ Source/Aura/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Mapton
+
+
+#include "CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
