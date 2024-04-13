32710c6b0756b81e66b77e554356da99f8489bb0
Mapton24 <robertosk555@gmail.com>
2024-04-13 13:10:53 +0200

    Blueprint changes for hit reacting

--- Source/Aura/Interaction/CombatInterface.h
+++ Source/Aura/Interaction/CombatInterface.h
@@ -28,5 +28,6 @@ public:
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
 	void UpdateFacingTarget(const FVector& Target);
 
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	UAnimMontage* GetHitReactMontage();
 };
