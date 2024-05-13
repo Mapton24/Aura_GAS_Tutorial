d614a11a7399aee508c00aa832db1911e5d5b3b7
Mapton24 <robertosk555@gmail.com>
2024-05-13 12:22:24 +0200

    Added AuraMeleeAttack c++

---
+++ Source/Aura/AbilitySystem/Abilities/AuraMeleeAttack.cpp
@@ -0,0 +1,5 @@
+// Copyright Mapton
+
+
+#include "AuraMeleeAttack.h"
+
---
+++ Source/Aura/AbilitySystem/Abilities/AuraMeleeAttack.h
@@ -0,0 +1,17 @@
+// Copyright Mapton
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraDamageGameplayAbility.h"
+#include "AuraMeleeAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraMeleeAttack : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
