9cc06a9aba98bd228d3c67786c37bf8498d5ffc4
Mapton24 <robertosk555@gmail.com>
2024-06-30 13:16:52 +0200

    Created a LevelUpInfo data asset

---
+++ Source/Aura/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,34 @@
+// Copyright Mapton
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+USTRUCT()
+struct FAuraLevelUpInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 LevelUpRequirement = 0;
+	UPROPERTY(EditDefaultsOnly)
+	int32 AttributePointReward = 1;
+	UPROPERTY(EditDefaultsOnly)
+	int32 SpellPointReward = 1;
+};
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FAuraLevelUpInfo> LevelUpInformation;
+
+	int32 FindLevelForExp(int32 XP);
+};
