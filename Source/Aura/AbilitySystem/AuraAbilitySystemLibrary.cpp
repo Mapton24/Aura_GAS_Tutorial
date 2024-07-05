0f4c6bdccf82014b699a25d43d77c114a59d535f
Mapton24 <robertosk555@gmail.com>
2024-07-05 14:11:46 +0200

    Created Function to get RewardForClassAndLevel inside ASLibrary

--- Source/Aura/AbilitySystem/AuraAbilitySystemLibrary.cpp
+++ Source/Aura/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -86,6 +86,18 @@ void UAuraAbilitySystemLibrary::GiveStartupAbilities(const UObject* WorldContext
 	}
 }
 
+int32 UAuraAbilitySystemLibrary::GetXPRewardForClassAndLevel(const UObject* WorldContextObject,
+	ECharacterClass CharacterClass, int32 CharacterLevel)
+{
+	UCharacterClassInfo* CharacterClassInfo = GetCharacterClassInfo(WorldContextObject);
+	if (CharacterClassInfo == nullptr) return 0;
+
+	const FCharacterClassDefaultInfo& Info = CharacterClassInfo->GetClassDefaultInfo(CharacterClass);
+	const float XPReward = Info.XPReward.GetValueAtLevel(CharacterLevel);
+
+	return static_cast<int32>(XPReward);
+
+}
 UCharacterClassInfo* UAuraAbilitySystemLibrary::GetCharacterClassInfo(const UObject* WorldContextObject)
 {
 	AAuraGameModeBase* AuraGameMode = Cast<AAuraGameModeBase>(UGameplayStatics::GetGameMode(WorldContextObject));
@@ -157,3 +169,5 @@ bool UAuraAbilitySystemLibrary::IsNotFriend(AActor* FirstActor, AActor* SecondAc
 	const bool Friends = BothArePlayers || BothAreEnemies;
 	return !Friends;
 }
+
+
