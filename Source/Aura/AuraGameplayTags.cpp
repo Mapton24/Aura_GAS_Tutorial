e02649da50649167d3caad63b384d9f2e16faa29
Mapton24 <robertosk555@gmail.com>
2024-08-16 15:34:44 +0200

    Game is now showing abilities in spell tree via tags

--- Source/Aura/AuraGameplayTags.cpp
+++ Source/Aura/AuraGameplayTags.cpp
@@ -90,43 +90,35 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	 */
 	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get().AddNativeGameplayTag(
 	FName("InputTag.LMB"),
-	FString("Input Tag for Left Mouse Button")
-	);
+	FString("Input Tag for Left Mouse Button"));
 
 	GameplayTags.InputTag_RMB = UGameplayTagsManager::Get().AddNativeGameplayTag(
 	FName("InputTag.RMB"),
-	FString("Input Tag for Right Mouse Button")
-	);
+	FString("Input Tag for Right Mouse Button"));
 
 	GameplayTags.InputTag_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
 	FName("InputTag.1"),
-	FString("Input Tag for 1 Key")
-	);
+	FString("Input Tag for 1 Key"));
 
 	GameplayTags.InputTag_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
 	FName("InputTag.2"),
-	FString("Input Tag for 2 Key")
-	);
+	FString("Input Tag for 2 Key"));
 
 	GameplayTags.InputTag_3 = UGameplayTagsManager::Get().AddNativeGameplayTag(
 	FName("InputTag.3"),
-	FString("Input Tag for 3 Key")
-	);
+	FString("Input Tag for 3 Key"));
 
 	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(
 	FName("InputTag.4"),
-	FString("Input Tag for 4 Key")
-	);
+	FString("Input Tag for 4 Key"));
 
 	GameplayTags.InputTag_Passive_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		FName("InputTag.Passive.1"),
-		FString("Input Tag Passive Ability 1")
-		);
+	FName("InputTag.Passive.1"),
+	FString("Input Tag Passive Ability 1"));
 
 	GameplayTags.InputTag_Passive_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		FName("InputTag.Passive.2"),
-		FString("Input Tag Passive Ability 2")
-		);
+	FName("InputTag.Passive.2"),
+	FString("Input Tag Passive Ability 2"));
 	
 	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag
 	(FName("Damage"),
@@ -217,6 +209,38 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	 FString("FireBolt Ability Tag")
 	);
 
+	GameplayTags.Abilities_HitReact = UGameplayTagsManager::Get().AddNativeGameplayTag
+	(FName("Abilities.Fire.HitReact"),
+	 FString("Hit React Ability")
+	);
+	GameplayTags.Abilities_Status_Eligible = UGameplayTagsManager::Get().AddNativeGameplayTag
+	(FName("Abilities.Status.Eligible"),
+	 FString("Eligible Status")
+	);
+	GameplayTags.Abilities_Status_Equipped = UGameplayTagsManager::Get().AddNativeGameplayTag
+	(FName("Abilities.Status.Equipped"),
+	 FString("Equipped Status")
+	);
+	GameplayTags.Abilities_Status_Locked = UGameplayTagsManager::Get().AddNativeGameplayTag
+	(FName("Abilities.Status.Locked"),
+	 FString("Locked Status")
+	);
+	GameplayTags.Abilities_Status_Unlocked = UGameplayTagsManager::Get().AddNativeGameplayTag
+	(FName("Abilities.Status.Unlocked"),
+	 FString("Unlocked Status")
+	);
+	GameplayTags.Abilities_Type_None = UGameplayTagsManager::Get().AddNativeGameplayTag
+	(FName("Abilities.Type.None"),
+	 FString("Type None")
+	);
+	GameplayTags.Abilities_Type_Offensive = UGameplayTagsManager::Get().AddNativeGameplayTag
+	(FName("Abilities.Type.Offensive"),
+	 FString("Type Offensive")
+	);
+	GameplayTags.Abilities_Type_Passive = UGameplayTagsManager::Get().AddNativeGameplayTag
+	(FName("Abilities.Type.Passive"),
+	 FString("Type Passive")
+	);
 	/*
 	* Cooldown
 	*/
