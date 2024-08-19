4117bcf14c095e669d12cfe868cb85c952d25ee2
Mapton24 <robertosk555@gmail.com>
2024-08-19 14:43:14 +0200

    Spell Points working properly on WBP

--- Source/Aura/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -5,10 +5,12 @@
 
 #include "Aura/AbilitySystem/AuraAbilitySystemComponent.h"
 #include "Aura/AbilitySystem/Data/AbilityInfo.h"
+#include "Aura/Player/AuraPlayerState.h"
 
 void USpellMenuWidgetController::BroadcastInitialValues()
 {
 	BroadcastAbilityInfo();
+	SpellPointsChanged.Broadcast(GetAuraPS()->GetSpellPoints());
 }
 
 void USpellMenuWidgetController::BindCallbacksToDependencies()
@@ -21,5 +23,10 @@ void USpellMenuWidgetController::BindCallbacksToDependencies()
 			Info.StatusTag = StatusTag;
 			AbilityInfoDelegate.Broadcast(Info);
 		}
+	});
+
+	GetAuraPS()->OnSpellPointsChangedDelegate.AddLambda([this](int32 SpellPoints)
+	{
+		SpellPointsChanged.Broadcast(SpellPoints);
 	});
 }
