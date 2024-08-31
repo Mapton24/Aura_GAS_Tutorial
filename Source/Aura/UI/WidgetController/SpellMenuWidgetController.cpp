4e6349855fec2d84d8d8672a2c1350424454b4da
Mapton24 <robertosk555@gmail.com>
2024-08-31 14:12:29 +0200

    Unbinded all the delegates inside our Attribute WBPs

--- Source/Aura/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -160,6 +160,8 @@ void USpellMenuWidgetController::OnAbilityEquipped(const FGameplayTag& AbilityTa
 	AbilityInfoDelegate.Broadcast(Info);
 
 	StopWaitingForEquipDelegate.Broadcast(AbilityInfo->FindAbilityInfoForTag(AbilityTag).AbilityTypeTag);
+	SpellGlobeReassignedDelegate.Broadcast(AbilityTag);
+	GlobeDeselect();
 }
 
 void USpellMenuWidgetController::ShouldEnableButtons(const FGameplayTag& AbilityStatus, int32 SpellPoints, bool& bShouldEnableSpellPointsButton, bool& bShouldEnableEquipButton)
