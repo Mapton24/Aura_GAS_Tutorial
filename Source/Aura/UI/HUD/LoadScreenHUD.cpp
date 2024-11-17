227adc0191b7ca8211f59e0978d8eb0a21c9c51a
Mapton24 <robertosk555@gmail.com>
2024-11-17 16:23:18 +0100

    Constructed a View Model

--- Source/Aura/UI/HUD/LoadScreenHUD.cpp
+++ Source/Aura/UI/HUD/LoadScreenHUD.cpp
@@ -3,3 +3,18 @@
 
 #include "LoadScreenHUD.h"
 
+#include "Aura/UI/ViewModel/MVVM_LoadScreen.h"
+#include "Aura/UI/Widget/LoadScreenWidget.h"
+#include "Blueprint/UserWidget.h"
+
+void ALoadScreenHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LoadScreenViewModel = NewObject<UMVVM_LoadScreen>(this, LoadScreenViewModelClass);
+	
+	LoadScreenWidget = CreateWidget<ULoadScreenWidget>(GetWorld(), LoadScreenWidgetClass);
+	LoadScreenWidget->AddToViewport();
+
+	
+}
