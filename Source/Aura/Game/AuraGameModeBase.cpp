d96cfb5aa70b026ea46414997b7c874d0289a8a9
Mapton24 <robertosk555@gmail.com>
2024-11-24 13:11:14 +0100

    Created Save game object

--- Source/Aura/Game/AuraGameModeBase.cpp
+++ Source/Aura/Game/AuraGameModeBase.cpp
@@ -3,3 +3,21 @@
 
 #include "AuraGameModeBase.h"
 
+#include "LoadScreenSaveGame.h"
+#include "Aura/UI/ViewModel/MVVM_LoadSlot.h"
+#include "Kismet/GameplayStatics.h"
+
+void AAuraGameModeBase::SaveSlotData(UMVVM_LoadSlot* LoadSlot, int32 SlotIndex)
+{
+	if (UGameplayStatics::DoesSaveGameExist(LoadSlot->LoadSlotName, SlotIndex))
+	{
+		UGameplayStatics::DeleteGameInSlot(LoadSlot->LoadSlotName, SlotIndex);
+	}
+	USaveGame* SaveGameObject = UGameplayStatics::CreateSaveGameObject(LoadScreenSaveGameClass);
+	ULoadScreenSaveGame* LoadScreenSaveGame = Cast<ULoadScreenSaveGame>(SaveGameObject);
+	LoadScreenSaveGame->PlayerName = LoadSlot->PlayerName;
+
+	UGameplayStatics::SaveGameToSlot(LoadScreenSaveGame, LoadSlot->LoadSlotName, SlotIndex);
+
+	
+}
