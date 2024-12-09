b068abf6066e66c8a6e8cfe3f5dffb3607ac038f
Mapton24 <robertosk555@gmail.com>
2024-12-09 14:09:34 +0100

    Fixed widget to show proper level

--- Source/Aura/UI/ViewModel/MVVM_LoadSlot.h
+++ Source/Aura/UI/ViewModel/MVVM_LoadSlot.h
@@ -45,9 +45,11 @@ public:
 
 	void SetPlayerName(FString InPlayerName);
 	void SetMapName(FString InMapName);
+	void SetPlayerLevel(int32 InLevel);
 
-	FString GetPlayerName() const {return PlayerName; }
-	FString GetMapName() const {return MapName; }
+	FString GetPlayerName() const { return PlayerName; }
+	FString GetMapName() const { return MapName; }
+	int32 GetPlayerLevel() const { return PlayerLevel; }
 
 private:
 		UPROPERTY(EditAnywhere, BlueprintReadWrite, FieldNotify, Setter, Getter, meta = (AllowPrivateAccess="true"));
@@ -55,4 +57,7 @@ private:
 	
 		UPROPERTY(EditAnywhere, BlueprintReadWrite, FieldNotify, Setter, Getter, meta = (AllowPrivateAccess="true"));
 		FString MapName;
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, FieldNotify, Setter, Getter, meta = (AllowPrivateAccess="true"));
+		int32 PlayerLevel;
 };
